9e2a29a42ba0f70782f9143539cad42139993441
Matt Herzfeld <matthew.herzfeld@auburn.edu>
2023-06-17 21:39:10 -0700

    Initial Commit

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by user239896 on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
